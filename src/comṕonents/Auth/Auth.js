a4696b8dab5aa96e6df8c62f32356b1e60308f2a
joscord <josecorderodev@gmail.com>
2022-03-05 13:34:30 -0300

    update: modified index.js to nest App.js inside Provider. Modified Auth.js

--- src/comṕonents/Auth/Auth.js
+++ src/comṕonents/Auth/Auth.js
@@ -1,23 +1,37 @@
 import classes from './Auth.module.css';
+// Importamos las acciones y la función de despacho de acciones
+import { authActions } from '../../store';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Auth = () => {
-  return (
-    <main className={classes.auth}>
-      <section>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
-          </div>
-          <button>Login</button>
-        </form>
-      </section>
-    </main>
-  );
+	// De nuestros hooks obtenemos las diferentes propiedades
+	const dispatch = useDispatch();
+	const isAuthenticated = useSelector(state => state.isAuthenticated);
+	// Destructuramos las acciones
+	const { login, logout } = authActions;
+
+	// Definimos una función para manejar el login
+	const loginHandler = () => {
+		dispatch(login());
+	};
+
+	return (
+		<main className={classes.auth}>
+			<section>
+				<form>
+					<div className={classes.control}>
+						<label htmlFor='email'>Email</label>
+						<input type='email' id='email' />
+					</div>
+					<div className={classes.control}>
+						<label htmlFor='password'>Password</label>
+						<input type='password' id='password' />
+					</div>
+					<button>Login</button>
+				</form>
+			</section>
+		</main>
+	);
 };
 
 export default Auth;
